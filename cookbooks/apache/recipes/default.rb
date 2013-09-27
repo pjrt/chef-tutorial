abfb9a1eba7167732d6db3dea81e5ce11f6813f3
Pedro Rodriguez <pedro@updater.com>
2013-09-27 18:18:08 -0400

    Half way throughstst

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -14,7 +14,45 @@ service "apache2" do
     action [:enable, :start]
 end
 
-cookbook_file "var/www/index.html" do
-    source "index.html"
-    mode "0644"
+execute "a2dissite default" do
+    only_if do
+        File.symlink?("/etc/apache2/sites-enabled/000-default")
+    end
+    notifies :restart, "service[apache2]"
 end
+
+node['apache']['sites'].each do |site_name, site_data|
+    document_root = "/src/apache/#{site_name}"
+
+    template "/etc/apache2/sites-available/#{site_name}" do
+        source "custom.erb"
+        mode "0644"
+        variables(
+            :document_root => document_root,
+            :port => site_data['port']
+        )
+        notifies :restart, "service[apache2]"
+    end
+
+    execute "a2ensite #{site_name}" do
+        not_if do
+            File.symlink?("/etc/apache2/sites-enabled/#{site_name}")
+        end
+        notifies :restart, "service[apache2]"
+    end
+
+    directory document_root do
+        mode "0755"
+        recursive true
+    end
+
+    template "#{document_root}/index.html" do
+        source "index.html.erb"
+        mode "0644"
+        variables(
+            :site_name => site_name,
+            :port => site_data['port']
+        )
+    end
+end
+
