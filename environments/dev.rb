b1d448746df2c84a86ad0a64c8d58126b24d4bd3
Pedro Rodriguez <pedro@updater.com>
2013-10-04 15:31:47 -0400

    Finished downloading cookbooks

---
+++ environments/dev.rb
@@ -0,0 +1,3 @@
+name "dev"
+description "For devs!!!"
+cookbook "apache", "= 0.2.0"
--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,11 @@
 name "base"
 description "Base Server Role"
-run_list "recipe[motd]", "recipe[users]"
+run_list "recipe[chef-client::delete_validation]", "recipe[motd]", "recipe[users]", "recipe[chef-client]", "recipe[ntp]"
+#def_run_list = ["recipe[motd]", "recipe[users]"]
+#env_run_lists({
+    #"_default" => def_run_list,
+    #"dev" => def_run_list,
+    #"production" => [
+        #"recipe[motd]"
+    #]
+#})
